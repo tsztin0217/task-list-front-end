b27916f1ff5303d8a10e9000f224bc4277771b20
Nicole <xixilele1990@gmail.com>
2025-12-21 23:34:20 -0800

    wrap input as an object

--- src/components/NewTaskForm.jsx
+++ src/components/NewTaskForm.jsx
@@ -2,40 +2,65 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 
-const NewTaskForm = ({ onAddTask }) => {
-    const [newTaskTitle, setNewTaskTitle] = useState('');
+const kDefaultFormState = {
+    title: '',
+    description: '',
+};
 
-    const handleNewTaskChange = (event) => {
-        setNewTaskTitle(event.target.value);
+const NewTaskForm = ({ onAddTask }) => {
+    const [formData, setFormData] = useState(kDefaultFormState);
+
+    const handleChange = (event) => {
+        const inputValue = event.target.value;
+        const inputName = event.target.name;
+
+        setFormData((prevFormData) => {
+            return {
+                ...prevFormData,
+                [inputName]: inputValue,
+            };
+        });
     };
 
     const handleAddTask = (event) => {
         event.preventDefault();
 
-        const trimmedTitle = newTaskTitle.trim();
-        if (!trimmedTitle) {          // if the title is empty, do not submit form
+        const trimmedTitle = formData.title.trim();
+        const trimmedDescription = formData.description.trim();
+
+        if (!trimmedTitle || !trimmedDescription) {
             return;
         }
 
         const newTask = {
             title: trimmedTitle,
-            description: 'Default description from frontend'// hardcode description here for now
+            description: trimmedDescription,
         };
+
         onAddTask(newTask);
-        setNewTaskTitle('');
+        setFormData(kDefaultFormState);
     };
 
-    return(
-
+    return (
         <form onSubmit={handleAddTask}>
-            <label htmlFor="new-task-title">New Task:</label>
+            <label htmlFor="new-task-title">New Task Title:</label>
             <input
                 type="text"
                 id="new-task-title"
-                name="new-task-title"
-                value={newTaskTitle}
-                onChange={handleNewTaskChange}
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
             />
+
+            <label htmlFor="new-task-description">New Task Description:</label>
+            <input
+                type="text"
+                id="new-task-description"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+            />
+
             <button type="submit" className="button">Add Task</button>
         </form>
     );
